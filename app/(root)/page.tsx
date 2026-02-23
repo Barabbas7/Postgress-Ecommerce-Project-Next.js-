fc4e818401a5c98ea94de29c7b154a0f7ce492b4
Daniel Kebede <barabbas218@gmail.com>
2026-02-23 13:51:56 +0300

    Root Layout(root) & Constants(.env)

---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+const Homepage = () => {
+  return <Button>Button</Button>;
+}
+ 
+export default Homepage;
